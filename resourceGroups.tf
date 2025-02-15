2e2cdc152ac05d31d678743b0f9fe61b9c3826e4
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-02-14 20:52:39 -0800

    Add RG and Windows analysis VM

--- resourceGroups.tf
+++ resourceGroups.tf
@@ -7,3 +7,7 @@ resource "azurerm_resource_group" "dc_rg" {
   name     = "soc-dc-demo-rg-cnc-01"
   location = var.location
 }
+resource "azurerm_resource_group" "analysis_rg" {
+  name     = "soc-analysis-demo-rg-cnc-01"
+  location = var.location
+}
