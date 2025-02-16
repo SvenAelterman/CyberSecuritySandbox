aab528ad15d9358eae339fc2ea2afc1f0b1d541e
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-02-16 15:39:56 -0600

    Deploy route tables only after firewall is deployed Add routes for KMS servers separately

--- routeTables.tf
+++ routeTables.tf
@@ -12,11 +12,16 @@ module "rt" {
       address_prefix         = "0.0.0.0/0"
       next_hop_type          = "VirtualAppliance"
       next_hop_in_ip_address = module.firewall.resource.ip_configuration[0].private_ip_address
-    }
-    kms = {
-      name             = "kms"
-      address_prefixes = ["40.83.235.53/32", "20.118.99.224/32"]
-      next_hop_type    = "Internet"
+    },
+    kms01 = {
+      name           = "kms01"
+      address_prefix = "40.83.235.53/32"
+      next_hop_type  = "Internet"
+    },
+    kms02 = {
+      name           = "kms02"
+      address_prefix = "20.118.99.224/32"
+      next_hop_type  = "Internet"
     }
   }
 
@@ -24,4 +29,6 @@ module "rt" {
     subnet1 = module.virtualnetwork.subnets[local.subnet_names.DomainControllerSubnet].resource.output.id,
     subnet2 = module.virtualnetwork.subnets[local.subnet_names.ComputeSubnet].resource_id
   }
+
+  depends_on = [module.firewall]
 }
