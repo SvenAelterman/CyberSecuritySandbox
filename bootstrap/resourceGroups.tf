467db46823dd19a9064bab7815a30f8ccaa02403
Vincent Balbarin <vbalbarin@microsoft.com>
2025-07-15 16:51:09 +0000

    Adding user assigned managed identity and roles

--- bootstrap/resourceGroups.tf
+++ bootstrap/resourceGroups.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "tfstate" {
   name     = local.resource_names["resource_group_state_name"]
   location = var.location
 }
+resource "azurerm_resource_group" "managed_id" {
+  name     = local.resource_names["resource_group_identity_name"]
+  location = var.location
+}
